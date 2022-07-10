eaec42b6700edbb2b1d0f868aa67503d06ac4158
Saket Gautam <81584480+saketgautam@users.noreply.github.com>
2022-07-10 12:26:52 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDGDXf84a0JzBM1IE3zjjhHIxlNSFBd5co",
+  authDomain: "authenticator-page.firebaseapp.com",
+  projectId: "authenticator-page",
+  storageBucket: "authenticator-page.appspot.com",
+  messagingSenderId: "374032883508",
+  appId: "1:374032883508:web:52b924f898f57d398dcf61",
+  measurementId: "G-JVDKBF6XC4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
