4a8a188c5ef1d988d51cac99de2e2a91d1aec156
Saket Gautam <81584480+saketgautam@users.noreply.github.com>
2022-07-12 13:40:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,14 +25,3 @@ function signUp(){
         document.getElementById("error").innerHTML = error.message
     });
 }
-
-function forgotPass(){
-    const email = document.getElementById("email").value
-    firebase.auth().sendPasswordResetEmail(email)
-    .then(() => {
-        alert("Reset link sent to your email id")
-    })
-    .catch((error) => {
-        document.getElementById("error").innerHTML = error.message
-    });
-}
